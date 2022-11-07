75823d7899442b31610ba8c041371de37b239517
dmitriy <ytdimagold752@gmail.com>
2022-11-07 18:16:53 +0500

    3 commit

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import cowsay
 
 
 def main():
+    cowsay.cow('Hello World')
+    cowsay.pig('yo')
     print("----------------------")
-    print("hhnhnhnhhnh")
+    print("keke")
 
 
 if __name__ == '__main__':
